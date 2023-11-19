7698306d3139be0363198c3522089b01f33bb396
ibrahimajifajarromadhon <ajii4262@gmail.com>
2023-11-19 08:32:26 +0700

    commit kedua

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card } from 'flowbite-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="w-100 bg-blue-700">OKE MASUK</div>
+    <Card href="#" className="max-w-sm">
+      <h5 className="text-2xl font-bold tracking-tight text-red-500 dark:text-white ">
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+      </p>
+    </Card>
+    </>
   );
 }
 
